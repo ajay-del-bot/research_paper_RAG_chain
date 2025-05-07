6529a2ef751482c0a67102ae91ddc4912b3fa66f
Ajay Rao <56514070+ajay-del-bot@users.noreply.github.com>
2025-05-06 23:59:41 -0500

    Update upload_file.py

--- src/upload_file.py
+++ src/upload_file.py
@@ -13,7 +13,7 @@ import os
 
 # Load environment variables
 load_dotenv()
-PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY2')
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
 GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
 index_name = os.environ.get('INDEX_NAME')
 
